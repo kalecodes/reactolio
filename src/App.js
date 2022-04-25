7b0e32fa17972c8dc9f353c7f49b536d2aea794b
kalen510 <kalen.wiley@gmail.com>
2022-04-24 22:39:39 -0400

    update projects

--- src/App.js
+++ src/App.js
@@ -24,19 +24,46 @@ function App() {
 
   const [projects] = useState([
     {
-      name: 'Handle - Events',
-      description: 'Example description 1',
-      image: 'handle.png'
-    },
-    {
       name: 'Crypto Today',
       description: 'Example description 2',
-      image: 'crypto.png'
+      image: 'crypto.png',
+      dlink: 'https://kalecodes.github.io/markets-today/',
+      rlink: 'https://github.com/kalecodes/markets-today'
     },
     {
-      name: 'Run Buddy',
+      name: 'Handle - Events',
+      description: 'Example description 1',
+      image: 'handle.png',
+      dlink: 'https://handle-events.herokuapp.com/',
+      rlink: 'https://github.com/kalecodes/raising-the-handlebars'
+    },
+    {
+      name: 'Weather Dashboard',
       description: 'Example description 3',
-      image: 'runbuddy.png'
+      image: 'weather.png',
+      dlink: 'https://kalecodes.github.io/weather-dashboard/',
+      rlink: 'https://github.com/kalecodes/weather-dashboard'
+    },
+    {
+      name: 'Notable',
+      description: 'Example description 3',
+      image: 'notable.png',
+      dlink: 'https://guarded-bastion-63529.herokuapp.com/',
+      rlink: 'https://github.com/kalecodes/notable'
+    },
+    {
+      name: 'Work Day Scheduler',
+      description: 'Example description 3',
+      image: 'schedulerr.png',
+      dlink: 'https://kalecodes.github.io/schedulerr/',
+      rlink: 'https://github.com/kalecodes/schedulerr'
+    },
+    {
+      name: 'Original Porfolio',
+      description: 'Example description 3',
+      image: 'ogportfolio.png',
+      dlink: 'https://kalecodes.github.io/portfolio/',
+      rlink: 'https://github.com/kalecodes/portfolio'
     }
   ])
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,7 +12,15 @@ function Project(props) {
                     <img src={require(`../../assets/images/${project.image}`)} className="col-xs-12 col-lg-9 img-thumbnail img-fluid" alt={project.name + ` ` + project.description}/>
                     <div className="col-lg-3">
                         <h3 className="" key={project.name}>{project.name}</h3>
-                        <p>{project.description}</p>    
+                        <p>{project.description}</p>
+                        <div className="row">
+                            <a href={project.rlink} target="_blank" rel="noopener noreferrer" className="col-6">
+                                <img className="img-fluid icon" src={require('../../assets/images/github.png')} alt='github icon - link to github repository'/>
+                            </a>
+                            <a href={project.dlink} target="_blank" rel="noopener noreferrer" className="col-6">
+                                <img className="img-fluid icon" src={require('../../assets/images/menu.png')} alt='nav stack icon - link to deployed app' />
+                            </a>
+                        </div>
                     </div>
                 </div>
             ))}
